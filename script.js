fcc5baf1cec3c7740ff2e7cd8008d06a4aae8b21
Syeda_Sima <simaakter089129@gmail.com>
2026-03-09 22:56:28 +0600

    updated script file

--- script.js
+++ script.js
@@ -19,6 +19,40 @@ function filterBtn(id){
     
     const getId = document.getElementById(id);
     getId.classList.add("bg-blue-700","text-white")
-
-   
 } 
+filterBtn("tabAll");
+
+
+
+
+const bugAndHelpLabels = (labels) => {
+
+    let newArr = labels.map((label) => {
+
+        let icon = "";
+
+        if (label === "bug") {
+            icon = `<i class="fa-solid fa-bug"></i>`;
+        }
+
+        if (label === "help wanted") {
+            icon = `<i class="fa-solid fa-handshake"></i>`;
+        }
+
+        if (label === "enhancement") {
+            icon = `<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+        }
+
+        return `<span class="flex items-center gap-1 font-semibold text-[10px] px-2 py-[2px] rounded-2xl border
+                ${label === "bug" ? "bg-red-100 text-red-400" :
+                label === "help wanted" ? "bg-gray-100 text-[#f59e0b]" :
+              "bg-[#defce8] text-[#00a96e]"}"> ${icon} ${label.toUpperCase()} </span>  `;
+
+
+    });
+
+    return newArr.join(" ");
+
+};
+
+
